677edec3cc24845cf43c16dc9fb9d5e357c052f0
James Garcia <conectis360@gmail.com>
2025-02-22 15:48:34 -0300

    feat: ref and useimperativeHandle

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import Input from "./components/Input";
 import Form from "./components/Form";
 import Button from "./components/Button";
+import { useRef } from "react";
 
 function App() {
+  const customForm = useRef(null);
+
   function handleSave(data: unknown) {
     const extractedData = data as { name: string; age: string };
     console.log(extractedData);
+    customForm.current?.clear();
   }
 
   return (
     <main>
-      <Form>
+      <Form onSave={handleSave} ref={customForm}>
         <Input label="Name" id="name" type="text"></Input>
         <Input label="Age" id="age" type="number"></Input>
         <p>
